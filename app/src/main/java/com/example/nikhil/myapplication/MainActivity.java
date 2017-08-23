76177e2ffa23c9866652168b563d8cd3e2b1c1d5
gadeelanikhil <gaddilanikhilreddy@gmail.com>
2017-08-24 00:55:32 +0530

    commit

---
+++ app/src/main/java/com/example/nikhil/myapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nikhil.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //hello this is nikhil
+        present();
+    }
+    public void present()
+    {
+        //helo this is nikhilreddy
+    }
+}
