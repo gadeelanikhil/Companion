7f6c95da1944eab1b13bfe15b0f2b45a465b4cc7
gadeelanikhil <gaddilanikhilreddy@gmail.com>
2017-08-24 01:05:07 +0530

    commit

--- app/src/main/java/com/example/nikhil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nikhil/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nikhil.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hello this is nikhil
+        // this is my new genetaed file
         present();
     }
     public void present()
     {
-        //helo this is nikhilreddy
+
+
     }
 }
